5bdf0ce2d236213daf51ab46104c7852d892eae1
vert7 <evertponce@outlook.com>
2019-11-07 16:04:07 +0000

    first push

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../../img/ScoutMaster.jpg";
 
 export class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-light bg-light mb-3">
+			<nav className="navbar navbar-light bg-light">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">
+						<img className="Logo-Nav" src={Logo} />
+					</span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn primary">Join now</button>
+						<button className="btn secondary">Sign in</button>
 					</Link>
 				</div>
 			</nav>
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
-
 import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import { Footer } from "./component/Footer";
 
 //create your first component
 export const Layout = () => {
