5b76382de56b079b4133ea706cf8b2751719abda
JpHendrix51 <jonathan.e.perez@gmail.com>
2019-12-07 22:55:06 +0000

    new paths in layout

--- src/js/layout.js
+++ src/js/layout.js
@@ -7,42 +7,47 @@ import { Discover } from "./views/discover";
 import { Registration } from "./views/registration";
 import { Artistprofile } from "./views/artistprofile";
 import Homefeed from "./component/home-feed";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/Footer";
 import { Login } from "./component/login";
 
 //create your first component
-export const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+export class Layout extends React.Component {
+	render() {
+		//the basename is used when your project is published in a subdirectory and not in the root of the domain
+		// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+		const basename = process.env.BASENAME || "";
 
-	return (
-		<div className="d-flex flex-column h-100">
-			<BrowserRouter>
-				<ScrollToTop>
-					<Navbar />
-
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/login" component={Login} />
-						<Route exact path="/discover" component={Discover} />
-						<Route exact path="/homefeed" component={Homefeed} />
-						<Route exact path="/registration" component={Registration} />
-						<Route exact path="/artistprofile" component={Artistprofile} />
-						<Route path="/demo" component={Demo} />
-						<Route path="/single/:theid" component={Single} />
-						<Route render={() => <h1>Not found!</h1>} />
-					</Switch>
-
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
-};
+		return (
+			<div className="d-flex flex-column h-100">
+				<BrowserRouter basename={basename}>
+					<ScrollToTop>
+						<Switch>
+							<Route path="/login" component={Login} />
+							<Route path="/registration" component={Registration} />
+							<Route
+								path="/"
+								render={() => (
+									<React.Fragment>
+										<Navbar />
+										<Switch>
+											<Route exact path="/" component={Home} />
+											<Route path="/discover" component={Discover} />
+											<Route path="/homefeed" component={Homefeed} />
+											<Route path="/artistsprofile" component={Artistprofile} />
+										</Switch>
+										<Footer />
+									</React.Fragment>
+								)}
+							/>
+							<Route render={() => <h1>It is okay :)</h1>} />
+						</Switch>
+					</ScrollToTop>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
 
 export default injectContext(Layout);
