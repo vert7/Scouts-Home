ef4f47f88e49ccf5c80990a77a21fc6d6ea17156
vert7 <evertponce@outlook.com>
2019-12-10 00:48:22 +0000

    nav login switch

--- src/js/component/home-feed.js
+++ src/js/component/home-feed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles/home-feed.scss";
 import SplitPane, { Pane } from "react-split-pane";
+import { FormGroup, Label, Input } from "reactstrap";
 import Avatar from "../../img/user-avatar.jpg";
 
 const Homefeed = () => {
@@ -9,7 +10,7 @@ const Homefeed = () => {
 			<SplitPane split="vertical">
 				<Pane initialSize="200px" className="pane-edit">
 					<div className="yout1">
-						<img src={Avatar} className="avatar" />
+						<img src={Avatar} className="avatar1" />
 						Profile
 					</div>
 					<div className="yout2">
@@ -31,6 +32,18 @@ const Homefeed = () => {
 				</Pane>
 				<Pane initialSize="25%" minSize="10%" maxSize="500px" className="pane-main">
 					<div className="crt-post">Create post</div>
+					<FormGroup className="d-flex justify-content-around align-items-center">
+						<Label for="exampleText" className="column" id="left-comment-col">
+							<img src={Avatar} className="avatar2" />
+						</Label>
+						<Input
+							type="textarea"
+							name="text"
+							id="right-comment-col"
+							placeholder="what's up?"
+							className="column"
+						/>
+					</FormGroup>
 				</Pane>
 			</SplitPane>
 		</div>
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,28 +1,67 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../img/Scout-Nav-Logo4.png";
+import { Context } from "../store/appContext.js";
 
 export class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-light bg-light">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">
-						<img className="Logo-Nav" src={Logo} />
-					</span>
-				</Link>
-				<Link to="/discover">
-					<input className="discoverbar" type="text" name="search" placeholder="Discover" />
-				</Link>
-				<div className="ml-auto">
-					<Link to="/registration">
-						<button className="btn btn-primary">Join now</button>
-					</Link>
-					<Link to="/login">
-						<button className="btn btn-secondary">Sign in</button>
-					</Link>
-				</div>
-			</nav>
+			<div>
+				<Context.Consumer>
+					{({ store }) => {
+						if (store.session.isLoggedIn === true) {
+							return (
+								<nav className="navbar navbar-light bg-light">
+									<Link to="/">
+										<span className="navbar-brand mb-0 h1">
+											<img className="Logo-Nav" src={Logo} />
+										</span>
+									</Link>
+									<Link to="/discover">
+										<input
+											className="discoverbar"
+											type="text"
+											name="search"
+											placeholder="Discover"
+										/>
+									</Link>
+									<div className="ml-auto">
+										<Link to="/homefeed">
+											<button className="btn btn-primary">Welcome!</button>
+										</Link>
+									</div>
+								</nav>
+							);
+						} else {
+							return (
+								<nav className="navbar navbar-light bg-light">
+									<Link to="/">
+										<span className="navbar-brand mb-0 h1">
+											<img className="Logo-Nav" src={Logo} />
+										</span>
+									</Link>
+									<Link to="/discover">
+										<input
+											className="discoverbar"
+											type="text"
+											name="search"
+											placeholder="Discover"
+										/>
+									</Link>
+									<div className="ml-auto">
+										<Link to="/registration">
+											<button className="btn btn-primary">Join now</button>
+										</Link>
+										<Link to="/login">
+											<button className="btn btn-secondary">Sign in</button>
+										</Link>
+									</div>
+								</nav>
+							);
+						}
+					}}
+				</Context.Consumer>
+			</div>
 		);
 	}
 }
