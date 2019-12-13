f74d13a4ec19f93339834842d7bf61b6e2a039f6
jlamazares <jlamazares01@gmail.com>
2019-12-13 03:34:00 +0000

    adding info to artist profile

--- src/js/component/artistbio.js
+++ src/js/component/artistbio.js
@@ -15,20 +15,14 @@ const Artistbio = () => {
 					<CardSubtitle>Influences:</CardSubtitle>
 				</strong>
 				<CardText>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					Biggie, Nas, Tupac, Coolio, Jay-Z, Future, T-Pain, The Strokes, Lil Wayne, A Tribe Called Quest,
+					Method Man, Snoop Dogg, Ice Cube, N.W.A.
 				</CardText>
 				<strong>
 					<CardSubtitle>Experience:</CardSubtitle>
 				</strong>
 				<CardText>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-					adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet,
-					consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+					10 years in the rap game and freestyling since the age of 16. Rap Battles are my forte.
 				</CardText>
 			</CardBody>
 		</Card>
